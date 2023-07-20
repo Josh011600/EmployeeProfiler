593affe3a059d2e80da7a4d7a4afc2b77dbb7063
Josh011600 <saculojosh1600@gmail.com>
2023-07-20 21:12:42 +0800

    last commit for this day

--- app.py
+++ app.py
@@ -1,53 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
 from twilio.rest import Client
 import random
 import sqlite3
 import os
-
-account_sid = os.environ.get('AC72e5e3ed30b51af85e21766d93c8377a')
-auth_token = os.environ.get('c5013c70815ed3b889bbbb94b1356d59')
-twilio_phone_number = os.environ.get('09610970818')
-
 app = Flask(__name__)
 
-# Twilio API credentials
-account_sid = 'your_account_sid'
-auth_token = 'your_auth_token'
-twilio_phone_number = 'your_twilio_phone_number'
-
-# Function to send SMS
-def send_sms(to_phone_number, message):
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body=message,
-        from_=twilio_phone_number,
-        to=to_phone_number
-    )
-    return message.sid
+# Placeholder user data for demonstration purposes
+users = {
+    'john': {
+        'password': 'password123',
+        'phone_number': '+1234567890'  # Replace with your phone number
+    }
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/send-verification-code', methods=['POST'])
-def send_verification_code():
-    # Retrieve the phone number from the HTML form
-    to_phone_number = request.form['phone_number']
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
 
-    # Generate a random 6-digit authentication code
-    authentication_code = str(random.randint(100000, 999999))
+    if username in users and users[username]['password'] == password:
+        return redirect(f'/verify/{username}/{password}')
+    else:
+        return "Invalid username or password."
 
-    # Compose the SMS message
-    sms_message = f'Your verification code is: {authentication_code}'
+@app.route('/verify/<username>/<password>', methods=['GET', 'POST'])
+def verify(username, password):
+    if request.method == 'GET':
+        return render_template('verification.html', username=username, password=password)
 
-    # Send the SMS
-    send_sms(to_phone_number, sms_message)
-
-    return 'Verification code sent successfully.'
+    # Verify the entered verification code (you can add your own verification logic)
+    # For this example, we assume the verification code is correct
+    return "Verification successful. You are now logged in."
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
