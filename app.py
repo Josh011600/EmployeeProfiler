8aa5be4cd6d86d8b7fb5546eb473d1e5d5e5bab4
Josh011600 <saculojosh1600@gmail.com>
2023-07-22 22:37:21 +0800

    first commit

--- app.py
+++ app.py
@@ -1,40 +1,19 @@
-# app.py
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-import bcrypt
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-db = SQLAlchemy(app)
-
-# Define the User model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(100))
-    role = db.Column(db.String(20))
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def display_data():
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
 
-@app.route('/login', methods=['POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    cursor.execute('SELECT * FROM users')
+    data = cursor.fetchall()
 
-        # Check if the username exists in the database
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-            # Redirect to the appropriate page based on the role
-            if user.role == 'admin':
-                return redirect(url_for('admin_page'))
-            elif user.role == 'user':
-                return redirect(url_for('user_page'))
+    conn.close()
 
-        # If the credentials are incorrect, show an error message
-        return 'Invalid username or password. Please try again.'
+    return render_template('index.html', data=data)
 
-# ... rest of the code remains the same ...
+if __name__ == '__main__':
+    app.run(debug=True)
