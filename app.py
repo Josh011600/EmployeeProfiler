f43d310d5649001ffbae0a7fdd787311f46aadd0
Josh011600 <saculojosh1600@gmail.com>
2023-08-27 18:09:07 +0800

    display employee informations to admin dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mail import Mail, Message
 import sqlite3
 from datetime import datetime
@@ -34,15 +34,51 @@ def validate_user(username_or_email, password):
     
     return user if user else None
 
+# Simulating user authentication
+def authenticate_user(username, password):
+    # Logic to authenticate user and fetch user ID
+    # Replace this with your actual authentication logic
+    if username == username and password == password:
+        return 1  # Return a user ID
+    return None
 
-@app.route('/')
-def index_page():
-    return render_template('index.html')
+
+
+def getEmployees():
+  conn = sqlite3.connect('mydatabase.db')
+  cursor = conn.cursor()
+  cursor.execute('SELECT * FROM employees')
+  employees = cursor.fetchall()
+  conn.close()
+  return employees
+
+def get_user_data(user_id):
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM employees WHERE id = ?', (user_id,))
+    user_data = cursor.fetchone()
+    conn.close()
+    return user_data
+
+
+def get_current_user_id():
+    return session.get('user_id')
+
+
+
+def getName():
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT name FROM employees')
+    name = cursor.fetchall()
+    conn.close()
+    return name[0] if name else None
 
 
 
 @app.route('/index', methods=['POST'])
 def index():
+    
     username = request.form['username']
     password = request.form['password']
 
@@ -52,13 +88,23 @@ def index():
         username, usertype = user
           # Access 'username' at index 2
         usertype = user[1] 
-        if usertype == 'admin':
+        if usertype:
             # Add the logic for admin login (e.g., session management, redirect to admin dashboard)
-            employees = display_table()
-            return render_template('admin.html', username=username, usertype=usertype)
-        elif usertype == 'employee':
+            user_id = authenticate_user(username, password)
+            employees = getEmployees()
+            name = getName()
+            '''user_id = get_current_user_id()''' # Replace this with your method to get the user's ID
+            user_data = get_user_data(user_id)
+            
+            if user_id:
+                session['id'] = user_id  # Store user ID in session
+                return render_template('admin.html', username=username, usertype=usertype, employees=employees, name=name, user_data=user_data)
+    elif usertype == 'employee':
             # Add the logic for user login (e.g., session management, redirect to user dashboard)
             return render_template('user.html', username=username, usertype=usertype)
+    
+        
+       
         
         
     # Add the logic for failed login (e.g., display an error message)
@@ -67,9 +113,10 @@ def index():
 
 
 
-
+'''
 @app.route('/')
 def display_table():
+
     conn = sqlite3.connect('mydatabase.db')
     cursor = conn.cursor()
     cursor.execute("SELECT id, name, age, email, dateOfBirth, usertype FROM employees") 
@@ -77,6 +124,8 @@ def display_table():
     conn.close()
 
     return render_template('admin.html', employees=employees)
+'''
+
 
 
 
@@ -97,7 +146,7 @@ def register_submit():
     password = request.form['password']
     dateOfBirth = request.form['date_of_birth']
     # Convert the date string to a datetime object
-    dateOfBirth = datetime.strptime(dateOfBirth, '%Y-%m-%d').date()
+    dateOfBirth = datetime.strptime(dateOfBirth, '%m-%d-%Y').date()
 
      # Check if the username or email already exists in the database
     cursor.execute("SELECT * FROM employees WHERE username = ? OR email = ?", (username, email))
@@ -127,7 +176,10 @@ def registration_failed():
     return render_template('registration_failed.html', reason=reason)
 
 
-
+#for logout purposes
+@app.route('/')
+def index_page():
+    return render_template('index.html')
 
 
 
